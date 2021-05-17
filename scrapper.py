43ef4288c4afab7877a597fce45baca133094138
egocentryczny <mgal7337@outlook.com>
2021-05-17 23:36:51 +0200

    new approach

--- scrapper.py
+++ scrapper.py
@@ -7,14 +7,17 @@ from bs4 import BeautifulSoup
 
 from time import sleep
 
-
-
 def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument("--start-maximized")
+    options.add_argument("--window-size=1920,1080")
     driver = webdriver.Remote(
         command_executor='http://localhost:4444/wd/hub',
-        desired_capabilities=DesiredCapabilities.CHROME
+        desired_capabilities=DesiredCapabilities.CHROME,
+        options=options
     )
     return driver
 
@@ -30,10 +33,9 @@ def connect_to_mzgov(browser):
             )
             print("Connected to MZ.GOV!")
             return True
-        except Exception as ex:
+        except Exception:
             connection_attempts += 1
             print(f'Error connecting to {base_url}')
-            print(ex)
             print(f'Attempt #{connection_attempts}.')
 
     return False
@@ -44,12 +46,12 @@ def connect_to_arcgis(browser, url):
         try:
             print("Connecting to ARCGIS...")
             browser.get(url)
-            sleep(15)
-            browser.find_element_by_id('app')
+            WebDriverWait(browser, 30).until(
+                EC.presence_of_element_located((By.TAG_NAME, "iframe"))
+            )
             print("Connected to ARCGIS!")
             return True
-        except Exception as ex:
-            print(str(ex))
+        except Exception:
             connection_attempts += 1
             print(f'Error connecting to {url}')
 
@@ -62,21 +64,58 @@ def parse_mz_html(html):
     return iframe_blocks[0].get('src')
 
 def parse_arcgis_html(html):
-    soup = soup = BeautifulSoup(html, 'html.parser')
-    dock_elements = soup.find_all(class_="dock-element")
-    for dock_element in dock_elements:
-        print(dock_element)
+    soup = BeautifulSoup(html, 'html.parser')
+    iframe_blocks = soup.find_all('iframe')
+    return iframe_blocks[0].get('src')
+
+def connect_to_rcb_gis(browser, url):
+    connection_attempts = 0
+    while connection_attempts < 1:
+        try:
+            print("Connecting to RCB GIS...")
+            browser.get(url)
+            for x in range(0,15):
+                sleep(30)
+                browser.get_screenshot_as_file(f'./{x}.png')        
+            # WebDriverWait(browser, 60).until(
+            #     EC.presence_of_element_located((By.CLASS_NAME, 'full-container'))
+            # )
+            print("Conntected to RCB GIS!")
+            return True
+        except Exception:
+            connection_attempts += 1
+            print(f'Error connecting to {url}')
+    return False
+
+def parse_rcb_gis(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    print(soup.prettify())
+    # iframe_blocks = soup.find_all('iframe')
+    # return iframe_blocks[0].get('src')
+
 
 
 if __name__ == '__main__':
     browser = get_driver()
+    next_url = ""
     if connect_to_mzgov(browser=browser):
         html = browser.page_source
-        arcgis_url = parse_mz_html(html)
-        print(arcgis_url)
-        if connect_to_arcgis(browser, arcgis_url):
-            html = browser.page_source
-            parse_arcgis_html(html)
-
+        next_url = parse_mz_html(html)
+        print(next_url)
     browser.quit()
+
+    browser = get_driver()
+    if connect_to_arcgis(browser=browser, url=next_url):
+        html = browser.page_source
+        next_url = parse_arcgis_html(html)
+        print(next_url)
+    browser.quit()
+
+    browser = get_driver()
+    if connect_to_rcb_gis(browser = browser, url = next_url):
+        html = browser.page_source
+        parse_rcb_gis(html)
+    browser.quit()
+
+    
 
